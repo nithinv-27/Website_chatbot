48d4ec66d49c21833d2f31f4485b113454708781
Taraka Nithin Vankala <nithintaraka.v@gmail.com>
2025-03-07 01:46:39 +0530

    llm needs improvement

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const chatbot_card = document.querySelector(".chatbot-card");
 const user_input = document.querySelector(".search");
 const query_form = document.querySelector(".query-form");
 const card_content = document.querySelector(".card-content");
+const btns = document.querySelectorAll(".btns button");
 
 chat_btn.addEventListener("click", ()=>{
     chat_btn.style.display = "none";
@@ -25,22 +26,39 @@ query_form.addEventListener("submit", async (event)=>{
     p.textContent = input_val;
     div.appendChild(p);
     card_content.appendChild(div);
+    don(input_val);
+    
+});
+
+btns.forEach(button =>{
+    button.addEventListener("click", (event)=>{
+        let div = document.createElement("div");
+        let p = document.createElement("p");
+        div.classList.add("user-chat");
+        p.textContent = event.target.textContent;
+        div.appendChild(p);
+        card_content.appendChild(div);
+        don(event.target.textContent);
+    })
+})
+
+async function don(input){
     const formData = new FormData();
-    formData.append("query", input_val);
+    formData.append("query", input);
     const res = await fetch("http://127.0.0.1:8000/chatbot", {
         method: "POST",
         body: formData
     });
     const res_data = await res.json();
 
-    if(res_data.link){
-        let my_div = document.createElement("div");
-        let p = document.createElement("p");
-        my_div.classList.add("bot-chat");
-        p.textContent = res_data.response;
-        my_div.appendChild(p);
-        card_content.appendChild(my_div);
+    let my_div = document.createElement("div");
+    let my_p = document.createElement("p");
+    my_div.classList.add("bot-chat");
+    my_p.textContent = res_data.response;
+    my_div.appendChild(my_p);
+    card_content.appendChild(my_div);
 
+    if(res_data.link){
         let new_div = document.createElement("div");
         let a = document.createElement("a");
         new_div.classList.add("bot-link");
@@ -48,16 +66,6 @@ query_form.addEventListener("submit", async (event)=>{
         a.textContent = res_data.intent;
         new_div.appendChild(a);
         card_content.appendChild(new_div);
-        console.log(res_data);
 
-    }else{
-        let my_div = document.createElement("div");
-        let p = document.createElement("p");
-        my_div.classList.add("bot-chat");
-        p.textContent = res_data.response;
-        my_div.appendChild(p);
-        card_content.appendChild(my_div);
-        console.log("dsdfd");
     }
-
-})
+}
